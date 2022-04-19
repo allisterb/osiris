b7ce8e4c85d7ef19c6dd70d70c250e2b69b6d8f3
Allister Beharry <allister.beharry@gmail.com>
2022-04-18 23:36:35 -0400

    Add some gsql and bigquery scripts.

---
+++ bigquery/select_events_actor1.sql
@@ -0,0 +1,41 @@
+declare start_date INT64 DEFAULT 20220000;
+declare end_date INT64 DEFAULT 20220420;
+
+select distinct
+    sha1(concat(
+        ifnull(Actor1Code,''), 
+        ifnull(Actor1Name, ''), 
+        ifnull(Actor1CountryCode, ''), 
+        ifnull(Actor1KnownGroupCode, ''), 
+        ifnull(Actor1EthnicCode, ''), 
+        ifnull(Actor1Religion1Code, ''),
+        ifnull(Actor1Religion2Code, ''), 
+        ifnull(Actor1Type1Code, ''), 
+        ifnull(Actor1Type2Code, ''), 
+        ifnull(Actor1Type3Code, ''), 
+        ifnull(Actor1Geo_Type, 0),
+        ifnull(Actor1Geo_FullName, ''), 
+        ifnull(Actor1Geo_CountryCode, ''), 
+        ifnull(Actor1Geo_ADM1Code, ''), 
+        ifnull(Actor1Geo_ADM2Code, ''), 
+        ifnull(Actor1Geo_Lat, 0.0), 
+        ifnull(Actor1Geo_Long, 0.0),
+        ifnull(Actor1Geo_FeatureID, '')
+    )) as ActorID,
+    Actor1Code as ActorCode, 
+    Actor1Name as ActorName, 
+    Actor1CountryCode as ActorCountryCode, 
+    Actor1KnownGroupCode as ActorKnownGroupCode, 
+    Actor1EthnicCode as ActorEthnicCode, 
+    nullif(trim(concat(ifnull(Actor1Religion1Code, ''), ' ', ifnull(Actor1Religion2Code, ''))), '')  as ActorReligion,
+    nullif(trim(concat(ifnull(Actor1Type1Code, ''), ' ', ifnull(Actor1Type2Code, ''), ' ', ifnull(Actor1Type3Code, ''))), '') as ActorType,
+    Actor1Geo_Type as ActorGeo_Type, 
+    Actor1Geo_FullName as ActorGeo_FullName, 
+    Actor1Geo_CountryCode as ActorGeo_CountryCode, 
+    nullif(trim(concat(ifnull(Actor1Geo_ADM1Code, ''), ' ', ifnull(Actor1Geo_ADM2Code, ''))), '') as ActorGeo_ADMCode, 
+    Actor1Geo_Lat as ActorGeo_Lat, 
+    Actor1Geo_Long as ActorGeo_Long, 
+    Actor1Geo_FeatureID as ActorGeo_FeatureID,
+    parse_date('%Y%m%d', cast(SQLDATE as string)) AS EventDate 
+    from `gdelt-bq.gdeltv2.events`
+    where SQLDATE > start_date and SQLDATE < end_date
---
+++ bigquery/select_events_actor2.sql
@@ -0,0 +1,42 @@
+declare start_date INT64 DEFAULT 20220000;
+declare end_date INT64 DEFAULT 20220420;
+
+select distinct
+    sha1(concat(
+        ifnull(Actor2Code,''), 
+        ifnull(Actor2Name, ''), 
+        ifnull(Actor2CountryCode, ''), 
+        ifnull(Actor2KnownGroupCode, ''), 
+        ifnull(Actor2EthnicCode, ''), 
+        ifnull(Actor2Religion1Code, ''),
+        ifnull(Actor2Religion2Code, ''), 
+        ifnull(Actor2Type1Code, ''), 
+        ifnull(Actor2Type2Code, ''), 
+        ifnull(Actor2Type3Code, ''), 
+        ifnull(Actor2Geo_Type, 0),
+        ifnull(Actor2Geo_FullName, ''), 
+        ifnull(Actor2Geo_CountryCode, ''), 
+        ifnull(Actor2Geo_ADM1Code, ''), 
+        ifnull(Actor2Geo_ADM2Code, ''), 
+        ifnull(Actor2Geo_Lat, 0.0), 
+        ifnull(Actor2Geo_Long, 0.0),
+        ifnull(Actor2Geo_FeatureID, '')
+    )) as ActorID,
+    Actor2Code as ActorCode, 
+    Actor2Name as ActorName, 
+    Actor2CountryCode as ActorCountryCode, 
+    Actor2KnownGroupCode as ActorKnownGroupCode, 
+    Actor2EthnicCode as ActorEthnicCode, 
+    nullif(trim(concat(ifnull(Actor2Religion1Code, ''), ' ', ifnull(Actor2Religion2Code, ''))), '')  as ActorReligion,
+    nullif(trim(concat(ifnull(Actor2Type1Code, ''), ' ', ifnull(Actor2Type2Code, ''), ' ', ifnull(Actor2Type3Code, ''))), '') as ActorType,
+    Actor2Geo_Type as ActorGeo_Type, 
+    Actor2Geo_FullName as ActorGeo_FullName, 
+    Actor2Geo_CountryCode as ActorGeo_CountryCode, 
+    nullif(trim(concat(ifnull(Actor2Geo_ADM1Code, ''), ' ', ifnull(Actor2Geo_ADM2Code, ''))), '') as ActorGeo_ADMCode, 
+    Actor2Geo_Lat as ActorGeo_Lat, 
+    Actor2Geo_Long as ActorGeo_Long, 
+    Actor2Geo_FeatureID as ActorGeo_FeatureID,
+    parse_date('%Y%m%d', cast(SQLDATE as string)) AS EventDate 
+    from `gdelt-bq.gdeltv2.events`
+    where SQLDATE > start_date and SQLDATE < end_date
+
