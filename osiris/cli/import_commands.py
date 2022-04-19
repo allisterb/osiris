267b654d0af5290e3bfa0de5675f439535d23cf3
Allister Beharry <allister.beharry@gmail.com>
2022-04-19 13:31:52 -0400

    Add import from bigquery.

--- osiris/cli/import_commands.py
+++ osiris/cli/import_commands.py
@@ -7,11 +7,11 @@ from rich import print
 from cli.commands import data_import
 from cli.util import *
 
-@data_import.command('gdelt', help='Import data from GDELT into a graph database server.')
+@data_import.command('gdelt', help='Import data from the GDELT file server into a graph database server.')
 @click.argument('table')
 @click.argument('start-date')
 @click.argument('end-date')
-@click.argument('target', default='tg')
+@click.argument('target', default='csv')
 def import_gdelt(table, start_date, end_date, target):
    from data.gdelt import DataSource
    gdelt = DataSource()
@@ -20,4 +20,14 @@ def import_gdelt(table, start_date, end_date, target):
    print(data.head())
 
 
+@data_import.command('bigquery', help='Import data from a Google BigQuery table into a graph database server.')
+@click.option('--google-app-creds', envvar="GOOGLE_APPLICATION_CREDENTIALS")
+@click.argument('bq-table')
+@click.argument('tg-table')
+@click.argument('target', default='csv')
+def import_bigquery(google_app_creds, bq_table, tg_table, target):
+   from google.cloud import bigquery
+   bqclient = bigquery.Client()
+   table = bigquery.TableReference.from_string(bq_table)
+   print(table.project)
    
