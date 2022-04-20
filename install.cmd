66881ce70aedff91424d23832d3c87ddaa0507ff
Allister Beharry <allister.beharry@gmail.com>
2022-04-20 19:09:17 -0400

    use own graphistry submodule.

--- install.cmd
+++ install.cmd
@@ -3,4 +3,5 @@ python -m ensurepip
 
 pip install wheel
 pip install ext\gdeltPyR
+pip install ext\graphistry
 pip install -r osiris\requirements.txt
--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ set -e
 
 pip install wheel
 pip install ext/gdeltPyR
+pip install ext/graphistry
 pip install -r osiris/requirements.txt
