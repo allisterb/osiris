540020ea1ee35253844a4e23f056f1c4a54f4e97
Allister Beharry <allister.beharry@gmail.com>
2022-04-27 20:50:19 -0400

    Add actors and mentions snapshots helpers.

--- osiris/data/tables.py
+++ osiris/data/tables.py
@@ -1,23 +1,45 @@
-"""Get shaped data"""
+"""Helpers to get tabular GDELT data from Google BigQuery"""
 from datetime import datetime
-from tqdm import tqdm
 
-from tqdm.auto import trange
 import pandas as pd
 
-import osiris_global
 from osiris_global import tqdm_iter
-from base.timer import begin
 from data.bigquery import DataSource
 
 bigquery = DataSource()
 
-def events(start_date:datetime, end_date:datetime=None, bs=10000, maxrows=None):
+def events(start_date:datetime, bs=10000, maxrows=None):
     table:str = ''
-    if start_date >= '20022-01-01':
-        table = "osiris-347701.gdelt_snapshots.events_actions_20220000_20220421"
+    if start_date >= '2022-01-01':
+        table = 'osiris-347701.gdelt_snapshots.events_actions_20220000_20220421'
+    elif start_date >= '2020-04-01':
+        table = 'osiris-347701.gdelt_snapshots.events_actions_20200400_'
+    else:
+        raise "An events table snapshot doesn't exist for this date range"
+
     data = bigquery.import_data("table", table, bs, maxrows)
-    return pd.concat(tqdm_iter(data, desc=f'Fetch events data in batches of {bs} rows', total = (int (maxrows / bs) if maxrows is not None else None), unit="batch"))
+    return pd.concat(tqdm_iter(data, desc=f'Fetch events table data', total = (int (maxrows / bs) if maxrows is not None else None), unit="batch"))
 
+def actors(start_date:datetime, bs=10000, maxrows=None):
+    table:str = ''
+    if start_date >= '2022-01-01':
+        table = 'osiris-347701.gdelt_snapshots.events_actors_20220000_'
+    elif start_date >= '2020-04-01':
+        table = 'osiris-347701.gdelt_snapshots.events_actors_20200400_'
+    else:
+        raise "An actors table snapshot doesn't exist for this date range"
+    
+    data = bigquery.import_data("table", table, bs, maxrows)
+    return pd.concat(tqdm_iter(data, desc=f'Fetch event actors table data', total = (int (maxrows / bs) if maxrows is not None else None), unit="batch"))
 
-
+def mentions(start_date:datetime, bs=10000, maxrows=None):
+    table:str = ''
+    if start_date >= '2022-01-01':
+        table = 'osiris-347701.gdelt_snapshots.events_mentions_20220400_'
+    elif start_date >= '2020-04-01':
+        table = 'osiris-347701.gdelt_snapshots.events_mentions_2020040000_'
+    else:
+        raise "A mentions table snapshot doesn't exist for this date range"
+    
+    data = bigquery.import_data("table", table, bs, maxrows)
+    return pd.concat(tqdm_iter(data, desc=f'Fetch event mentions table data', total = (int (maxrows / bs) if maxrows is not None else None), unit="batch"))
